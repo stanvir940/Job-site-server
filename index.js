061c155da847bdac0b2f0c643f21f7b63e7f6074
stanvir940 <hmtanvir940@gmail.com>
2024-11-01 21:20:14 +0600

    user based upload done

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { connectDB, mongoURI } = require("./db");
 const mongoose = require("mongoose");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
+
 app.use(cors());
 
 // Middleware for parsing JSON
@@ -46,17 +47,41 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
+// getting user id
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./firebase/job-site-f3932-firebase-adminsdk-yn3tj-c83834d58f.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
 app.post("/submit", async (req, res) => {
   // const { title, technology, partners, description } = req.body;
   const research = req.body;
   // const newProject = new Project({ title, technology, partners, description });
 
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).send("Authorization header missing");
+  }
+
+  const token = authHeader.split(" ")[1]; // Bearer token
+
   try {
+    const decodedToken = await admin.auth().verifyIdToken(token);
+    const userId = decodedToken.uid;
     // await newProject.save();
-    await researchCollection.insertOne(research);
+    // console.log("User ID:", userId, "Data:", req.body.data);
+    await researchCollection.insertOne({ ...research, userId });
+
     res.status(201).send({ message: "Project saved successfully" });
   } catch (error) {
-    res.status(500).send({ error: "Failed to save project" });
+    console.error("Error saving project:", error);
+    res
+      .status(500)
+      .send({ error: "Failed to save project", details: error.message });
   }
 });
 
